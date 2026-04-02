9e4805806302fafe1e107a37e0bc7c6754ee4832
Ezika trust chidi <trustezika831@gmail.com>
2026-04-02 14:26:28 +0100

    refactor(phonebook): use relative API base and normalize responses

--- part-2/phonebook/src/services/persons.js
+++ part-2/phonebook/src/services/persons.js
@@ -1,30 +1,66 @@
 import axios from "axios";
 
-// Base URL for the persons resource in the backend.
-const baseUrl = "http://localhost:3001/api/persons";
+// Base URL for the backend API.
+const baseUrl = "/api/persons";
 
 // Fetch all persons from the backend.
 const getAll = () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return request.then((response) => {
+    const data = response.data;
+    if (Array.isArray(data)) {
+      return data;
+    }
+    if (Array.isArray(data?.data)) {
+      return data.data;
+    }
+    if (Array.isArray(data?.persons)) {
+      return data.persons;
+    }
+    return [];
+  });
 };
 
 // Create a new person in the backend.
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+  return request.then((response) => {
+    const data = response.data;
+    if (data?.data) {
+      return data.data;
+    }
+    if (data?.person) {
+      return data.person;
+    }
+    return data;
+  });
 };
 
 // Update an existing person by id in the backend.
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+  return request.then((response) => {
+    const data = response.data;
+    if (data?.data) {
+      return data.data;
+    }
+    if (data?.person) {
+      return data.person;
+    }
+    return data;
+  });
 };
 
 // Delete a person by id from the backend.
 const remove = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
+  return request.then((response) => {
+    const data = response.data;
+    if (data?.data) {
+      return data.data;
+    }
+    return data;
+  });
 };
 
 export default { getAll, create, update, remove };
